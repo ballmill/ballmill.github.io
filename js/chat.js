670752bb074320890438483bdbfe29dc8024e306
ballmill <gengerxuan@shibangcrusher.com>
2018-09-13 16:46:20 +0800

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 document.writeln("<div style=\"width:160px;height:245px;float:right; z-index:999;position:fixed !important; top/**/:320px; position:absolute; top:expression(offsetParent.scrollTop+400);right:0;\"> ");
 document.writeln("<div id=\"lpButDivID-1331955599520\"></div>");
-document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en\"></script>");
+document.writeln("<script language=\"javascript\" src=\"https://mnn.zoosnet.net/JS/LsJS.aspx?siteid=MNN94995150&float=1&lng=en\"></script>");
 document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"><img src=\"images/chat.gif\" border=\"0\"></a>");
 document.writeln("</div>");
 
